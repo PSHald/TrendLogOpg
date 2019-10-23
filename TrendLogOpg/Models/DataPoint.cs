1d500d0e3f2f7f67cef29b90cb6ae0cc24c931e4
PSHald <48911827+PSHald@users.noreply.github.com>
2019-10-23 23:13:42 +0200

    Add files via upload

---
+++ TrendLogOpg/Models/DataPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrendLogOpg.Models
+{
+    public class DataPoint
+    {
+        public int PointID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int Value { get; set; }
+    }
+}
